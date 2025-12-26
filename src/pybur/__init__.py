839a18543a77bff8708541ce3e42d2f507414e3e
Jacob <jacob@ezekia.com>
2025-12-26 14:57:43 +0000

    adding basic app

--- src/pybur/__init__.py
+++ src/pybur/__init__.py
@@ -1,2 +1,17 @@
+import typer
+
+app = typer.Typer(no_args_is_help=True)
+
+
+@app.command()
+def build() -> None:
+    print("Building...")
+
+
+@app.command()
+def run() -> None:
+    print("Running....")
+
+
 def main() -> None:
-    print("Hello from pybur!")
+    app()
